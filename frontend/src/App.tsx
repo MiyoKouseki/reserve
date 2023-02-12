1ec778ec063772e441ed1b35e899ab2a45313d3b
MiyoKouseki <kouseki.miyo@gmail.com>
2023-02-12 13:55:59 +0900

    日本語対応したtoolbarを追加。

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,50 +1,33 @@
 import * as React from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
-import { DataGrid, GridColDef, GridValueGetterParams ,GridToolbar} from '@mui/x-data-grid';
-import './App.css';
-
-const columns: GridColDef[] = [
-  { field: 'id', headerName: 'ID', width: 90 },
-  {
-    field: 'Name',
-    headerName: '項目名',
-    width: 150,
-    editable: true,
-  },
-  {
-    field: 'status',
-    headerName: '使用状況',
-    width: 150,
-    editable: true,
-  },
-];
-
-const rows = [
-  { id: 1, Name: '体育館', status: 'available'},
-  { id: 2, Name: '卓球室', status: 'available'},
-  { id: 3, Name: '会議室', status: 'used'},
-  { id: 4, Name: '会議室2', status: 'available'},
-];
+import { DataGrid, GridColDef, GridToolbar, jaJP} from '@mui/x-data-grid';
+import { createTheme, ThemeProvider } from '@mui/material/styles'
 
 function App() {
+  const columns: GridColDef[] = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    { field: 'Name',headerName: '項目名',width: 150},
+    { field: 'status', headerName: '使用状況', width: 150},
+  ];
+  
+  const rows = [
+    { id: 1, Name: '体育館', status: 'available'},
+    { id: 2, Name: '卓球室', status: 'available'},
+    { id: 3, Name: '会議室', status: 'used'},
+    { id: 4, Name: '会議室2', status: 'available'},
+  ];
+
   return (
-    <Box sx={{ height: 400, width: '60%'}}>
+    <Box sx={{ height: 600, width: '100%'}}>
       <DataGrid
-        rows={rows}
-        columns={columns}
+        rows={rows}　columns={columns}
         pageSize={5}
         rowsPerPageOptions={[5]}
         checkboxSelection
         disableSelectionOnClick
         experimentalFeatures={{ newEditingApi: true }}
-        components={{ Toolbar: GridToolbar }} 
+        components={{ Toolbar: GridToolbar }}
+        localeText={jaJP.components.MuiDataGrid.defaultProps.localeText} 
       />
     </Box>
   );
